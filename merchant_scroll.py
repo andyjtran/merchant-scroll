223ad27cce9c9d9d156a86cc943116b3bd3b74fa
andyjtran <code@andyjtran.com>
2024-05-07 21:20:36 -0700

    Call Scryfall API to append collector number to relevant tokens

--- merchant_scroll.py
+++ merchant_scroll.py
@@ -7,17 +7,12 @@ df = pd.read_csv('/home/andy/Downloads/moxfield_haves.csv')
 # Remove art cards, Jumpstart theme cards, and 30th Anniversary Play Promo cards
 df = df[~((df['Edition'].str.len() == 4) & (df['Edition'].str.startswith('a') | df['Edition'].str.startswith('f') | df['Edition'].str.contains('p30a')))]
 
-# Card Kingdom assigned a letter to basic lands before Ikoria: Lair of Behemoths (IKO) / Q2 2020 for sets with multiple basic lands of the same type. 
-# Card Kingdom assigns letters in alphabetical order from least to greatest collector number for basic lands of the same type in the same set
-
-editions_with_assigned_letters = ['eld', 'war', 'm19', 'm20']
+# Basic Lands
+# Scryfall API to count unique prints of basic lands for relevant sets
 basic_land = ['Plains','Island','Swamp','Mountain','Forest']
-
-# Call Scryfall API to assign letter to basic lands
 scryfall_url = "https://api.scryfall.com/cards/search"
-
-cards_data = []
-
+scryfall_land = []
+editions_with_assigned_letters = ['eld', 'war', 'm19', 'm20']
 for edition in editions_with_assigned_letters:
     for land in basic_land:
         query = f'set:{edition} name:"{land}" unique:prints'
@@ -30,7 +25,7 @@ for edition in editions_with_assigned_letters:
                 has_more = response_json['has_more']
                 page_url = response_json['next_page'] if has_more else ''
                 for card in response_json['data']:
-                    cards_data.append({
+                    scryfall_land.append({
                         'Name': card['name'],
                         'Edition': card['set'],
                         'Collector Number': card['collector_number']
@@ -38,27 +33,50 @@ for edition in editions_with_assigned_letters:
             else:
                 has_more = False
 
-df_scry = pd.DataFrame(cards_data)
-df_scry = df_scry.sort_values(by=['Name','Edition', 'Collector Number'])
+df_land = pd.DataFrame(scryfall_land)
+df_land = df_land.sort_values(by=['Name','Edition', 'Collector Number'])
 
-# Create letters based on Scryfall data
 def assign_letters(group):
     group['Letter'] = [ascii_uppercase[i] for i in range(len(group))]
     return group
 
-df_land_letter = df_scry.groupby(['Name','Edition'], as_index=True).apply(assign_letters, include_groups=False)
+df_land_letter = df_land.groupby(['Name','Edition'], as_index=True).apply(assign_letters, include_groups=False)
 df_land_letter = df_land_letter.reset_index(drop=False)
-
-# Format and merge Scryfall data with Moxfield data 
 df_land_letter['Collector Number'] = df_land_letter['Collector Number'].astype(str)
 df = pd.merge(df, df_land_letter, on=['Name','Edition', 'Collector Number'], how='left')
-
-# Assign letters
 df.loc[pd.notna(df['Letter']), 'Collector Number'] = df['Collector Number'].astype(str) + ' ' + df['Letter'].fillna('')
 df = df.drop(columns=['Letter'])
 
-# Change collector number format.
-editions_with_three_digit_collector_number = ['afr', 'bro', 'brr', 'dmu', 'eld', 'j22', 'm19', 'm20', 'm21', 'neo', 'one', 'slx', 'snc', 'vow', 'war']
+# Tokens
+# Scryfall API to count unique prints of tokens in each set
+token_edition = df[(df['Edition'].str.len() == 4) & (df['Edition'].str.startswith('t'))]
+scryfall_token = []
+
+for edition, name in zip(token_edition['Edition'], token_edition['Name']):
+    query = f'set:{edition} name:"{name}" unique:prints'
+    has_more = True
+    page_url = scryfall_url
+    while has_more:
+        response = requests.get(page_url, params={'q': query})
+        if response.status_code == 200:
+            response_json = response.json()
+            has_more = response_json['has_more']
+            page_url = response_json['next_page'] if has_more else ''
+            for card in response_json['data']:
+                scryfall_token.append({
+                    'Name': card['name'],
+                    'Edition': card['set'],
+                    'Collector Number': card['collector_number']
+                })
+        else:
+            has_more = False
+
+df_token = pd.DataFrame(scryfall_token)
+unique_token = df_token.groupby(['Name', 'Edition'])['Collector Number'].nunique().reset_index(name='Unique Count')
+df = pd.merge(df, unique_token, on=['Name','Edition'], how='left')
+
+# Collector Number
+editions_with_three_digit_collector_number = ['afr', 'bro', 'brr', 'dmu', 'eld', 'j22', 'm19', 'm20', 'm21', 'neo', 'tneo', 'one', 'slx', 'snc', 'tsnc', 'vow', 'tvow', 'war']
 
 def format_collector_number(row):
     if row['Edition'] in editions_with_three_digit_collector_number:
@@ -82,25 +100,21 @@ def format_collector_number(row):
     
 df['Collector Number'] = df.apply(format_collector_number, axis=1)
 
-# Append 'token' to token card names and trim 't' from token card edition 
+# Append collector number to basic lands
+df.loc[df['Name'].isin(basic_land),'Name'] = df['Name'] + ' ' + df['Collector Number'].astype(str)
+# Append collector number to tokens
 token_str = ' Token '
-df['Name'] = df.apply(lambda x: x['Name'] + token_str + x['Collector Number'] if (len(x['Edition']) == 4 and x['Edition'].startswith('t')) else x['Name'], axis=1)
+df['Name'] = df.apply(lambda x: x['Name'] + token_str if (len(x['Edition']) == 4 and x['Edition'].startswith('t')) else x['Name'], axis=1)
+df['Name'] = df.apply(lambda x: x['Name'] + x['Collector Number'] if (len(x['Edition']) == 4 and x['Edition'].startswith('t') and x['Unique Count'] > 1) else x['Name'], axis=1)
+# Append collector number to bonus sheet
+df.loc[df['Edition'].isin(['wot']),'Name'] = df['Name'] + ' ' + df['Collector Number'].astype(str)
+df.loc[df['Edition'].isin(['otp']),'Name'] = df['Name'] + ' ' + df['Collector Number'].astype(str).str.slice(0, 5) + ' - Showcase)'
+# Trim names for double-faced cards
+mask = ~df['Name'].str.contains('Token')
+df.loc[mask, 'Name'] = df.loc[mask, 'Name'].str.split('//').str[0]
 df['Edition'] = df['Edition'].apply(lambda x: x[1:] if (len(x) == 4 and x.startswith('t')) else x)
 
-# Append collector number to land and bonus sheet card names
-df.loc[df['Name'].isin(basic_land),'Name'] = df['Name'] + ' ' + df['Collector Number'].astype(str)
-
-df.loc[df['Edition'].isin(['wot']),'Name'] = df['Name'] + ' ' + df['Collector Number'].astype(str)
-
-df.loc[df['Edition'].isin(['otp']),'Name'] = df['Name'] + ' ' + df['Collector Number'].astype(str).str.slice(0, 5) + ' - Showcase)'
-
-# Trim names for split and double-faced cards
-df['Name'] = df['Name'].str.split("//").str[0]
-
 df.rename(columns={'Name': 'Title', 'Count': 'Qty'}, inplace=True)
-
 cardkingdom_column_subset = ['Title','Edition','Foil','Qty']
-
 df = df[cardkingdom_column_subset]
-
 df.to_csv('/home/andy/Downloads/cardkingdom.csv', index=False)
